a140fde32a99ba0a360f0afcc91c2281d15df47a
neetaro <neet.utsu.taro@gmail.com>
2017-10-07 15:56:44 +0900

    a

--- atcoder-template.js
+++ atcoder-template.js
@@ -6,25 +6,22 @@ $(function(){
 	var div = ["#A-ques-div","#B-ques-div","#C-ques-div","#D-ques-div",
 					"#A-ans-div","#B-ans-div","#C-ans-div","#D-ans-div",
 					"#A-re-div","#B-re-div","#C-re-div","#D-re-div"];
-	var end= ["#A-ques-end","#B-ques-end","#C-ques-end","#D-ques-end",
-					"#A-ans-end","#B-ans-end","#C-ans-end","#D-ans-end",
-					"#A-re-end","#B-re-end","#C-re-end","#D-re-end"];
-	var close = ["#A-ques-close","#B-ques-close","#C-ques-close","#D-ques-close",
-					"#A-ans-close","#B-ans-close","#C-ans-close","#D-ans-close",
-					"#A-re-close","#B-re-close","#C-re-close","#D-re-close"];
 
     var linkText = "ここをクリックすると閉じます";
 
+    var nextHeader = "h3,h2,h1";
+
+    var closeLinkClass = "close-link";
 /*
     for(var i=0;i<h.length;i++){
 		console.log('$(h['+i+']).get('+i+').onclick = function(){var d = document.getElementById(div['+i+']);if(d.style.display == "none"){d.style.display = "block";}else{d.style.display = "none";}};');
     }
     *//*
     for(var i=0;i<h.length;i++){
-    	console.log("$(h["+i+"]).nextUntil(end["+i+"]).wrapAll(\"<div id='\"+div["+i+"]+\"'>\");");
+    	console.log("$(h["+i+"]).nextUntil(nextHeader).wrapAll(\"<div id='\"+div["+i+"]+\"'>\");");
     }*//*
     for(var i=0;i<h.length;i++){
-    	console.log('$(h['+i+']).nextUntil(end['+i+']).last().append("<p id=\'"+close['+i+']+"\' onclick=\'document.getElementById(\""+div['+i+']+"\").style.display=\"none\";\'><a href=\'"+div['+i+']+"\'>"+linkText+"</a></p>");');
+    	console.log('$(h['+i+']).nextUntil(nextHeader).last().append("<p class='closeLinkClass' onclick=\'document.getElementById(\""+div['+i+']+"\").style.display=\"none\";\'><a href=\'"+div['+i+']+"\'>"+linkText+"</a></p>");');
     }*/
 
 	if($(h[0]).get(0))$(h[0]).get(0).onclick = function(){var d = document.getElementById(div[0]);if(d.style.display == "none"){d.style.display = "block";}else{d.style.display = "none";}};
@@ -41,32 +38,32 @@ $(function(){
 	if($(h[11]).get(0))$(h[11]).get(0).onclick = function(){var d = document.getElementById(div[11]);if(d.style.display == "none"){d.style.display = "block";}else{d.style.display = "none";}};
 	
 
-	$(h[0]).nextUntil("h3,h2,h1").wrapAll("<div id='"+div[0]+"' style='display:none'>");
-	$(h[1]).nextUntil("h3,h2,h1").wrapAll("<div id='"+div[1]+"' style='display:none'>");
-	$(h[2]).nextUntil("h3,h2,h1").wrapAll("<div id='"+div[2]+"' style='display:none'>");
-	$(h[3]).nextUntil("h3,h2,h1").wrapAll("<div id='"+div[3]+"' style='display:none'>");
-	$(h[4]).nextUntil("h3,h2,h1").wrapAll("<div id='"+div[4]+"' style='display:none'>");
-	$(h[5]).nextUntil("h3,h2,h1").wrapAll("<div id='"+div[5]+"' style='display:none'>");
-	$(h[6]).nextUntil("h3,h2,h1").wrapAll("<div id='"+div[6]+"' style='display:none'>");
-	$(h[7]).nextUntil("h3,h2,h1").wrapAll("<div id='"+div[7]+"' style='display:none'>");
-	$(h[8]).nextUntil("h3,h2,h1").wrapAll("<div id='"+div[8]+"' style='display:none'>");
-	$(h[9]).nextUntil("h3,h2,h1").wrapAll("<div id='"+div[9]+"' style='display:none'>");
-	$(h[10]).nextUntil("h3,h2,h1").wrapAll("<div id='"+div[10]+"' style='display:none'>");
-	$(h[11]).nextUntil("h3,h2,h1").wrapAll("<div id='"+div[11]+"' style='display:none'>");
+	$(h[0]).nextUntil(nextHeader).wrapAll("<div id='"+div[0]+"' style='display:none'>");
+	$(h[1]).nextUntil(nextHeader).wrapAll("<div id='"+div[1]+"' style='display:none'>");
+	$(h[2]).nextUntil(nextHeader).wrapAll("<div id='"+div[2]+"' style='display:none'>");
+	$(h[3]).nextUntil(nextHeader).wrapAll("<div id='"+div[3]+"' style='display:none'>");
+	$(h[4]).nextUntil(nextHeader).wrapAll("<div id='"+div[4]+"' style='display:none'>");
+	$(h[5]).nextUntil(nextHeader).wrapAll("<div id='"+div[5]+"' style='display:none'>");
+	$(h[6]).nextUntil(nextHeader).wrapAll("<div id='"+div[6]+"' style='display:none'>");
+	$(h[7]).nextUntil(nextHeader).wrapAll("<div id='"+div[7]+"' style='display:none'>");
+	$(h[8]).nextUntil(nextHeader).wrapAll("<div id='"+div[8]+"' style='display:none'>");
+	$(h[9]).nextUntil(nextHeader).wrapAll("<div id='"+div[9]+"' style='display:none'>");
+	$(h[10]).nextUntil(nextHeader).wrapAll("<div id='"+div[10]+"' style='display:none'>");
+	$(h[11]).nextUntil(nextHeader).wrapAll("<div id='"+div[11]+"' style='display:none'>");
 
 
-	$(h[0]).nextUntil("h3,h2,h1").last().append("<p id='"+close[0]+"' onclick='document.getElementById(\""+div[0]+"\").style.display=\"none\";'><a href='"+div[0]+"'>"+linkText+"</a></p>");
-	$(h[1]).nextUntil("h3,h2,h1").last().append("<p id='"+close[1]+"' onclick='document.getElementById(\""+div[1]+"\").style.display=\"none\";'><a href='"+div[1]+"'>"+linkText+"</a></p>");
-	$(h[2]).nextUntil("h3,h2,h1").last().append("<p id='"+close[2]+"' onclick='document.getElementById(\""+div[2]+"\").style.display=\"none\";'><a href='"+div[2]+"'>"+linkText+"</a></p>");
-	$(h[3]).nextUntil("h3,h2,h1").last().append("<p id='"+close[3]+"' onclick='document.getElementById(\""+div[3]+"\").style.display=\"none\";'><a href='"+div[3]+"'>"+linkText+"</a></p>");
-	$(h[4]).nextUntil("h3,h2,h1").last().append("<p id='"+close[4]+"' onclick='document.getElementById(\""+div[4]+"\").style.display=\"none\";'><a href='"+div[4]+"'>"+linkText+"</a></p>");
-	$(h[5]).nextUntil("h3,h2,h1").last().append("<p id='"+close[5]+"' onclick='document.getElementById(\""+div[5]+"\").style.display=\"none\";'><a href='"+div[5]+"'>"+linkText+"</a></p>");
-	$(h[6]).nextUntil("h3,h2,h1").last().append("<p id='"+close[6]+"' onclick='document.getElementById(\""+div[6]+"\").style.display=\"none\";'><a href='"+div[6]+"'>"+linkText+"</a></p>");
-	$(h[7]).nextUntil("h3,h2,h1").last().append("<p id='"+close[7]+"' onclick='document.getElementById(\""+div[7]+"\").style.display=\"none\";'><a href='"+div[7]+"'>"+linkText+"</a></p>");
-	$(h[8]).nextUntil("h3,h2,h1").last().append("<p id='"+close[8]+"' onclick='document.getElementById(\""+div[8]+"\").style.display=\"none\";'><a href='"+div[8]+"'>"+linkText+"</a></p>");
-	$(h[9]).nextUntil("h3,h2,h1").last().append("<p id='"+close[9]+"' onclick='document.getElementById(\""+div[9]+"\").style.display=\"none\";'><a href='"+div[9]+"'>"+linkText+"</a></p>");
-	$(h[10]).nextUntil("h3,h2,h1").last().append("<p id='"+close[10]+"' onclick='document.getElementById(\""+div[10]+"\").style.display=\"none\";'><a href='"+div[10]+"'>"+linkText+"</a></p>");
-	$(h[11]).nextUntil("h3,h2,h1").last().append("<p id='"+close[11]+"' onclick='document.getElementById(\""+div[11]+"\").style.display=\"none\";'><a href='"+div[11]+"'>"+linkText+"</a></p>");
+	$(h[0]).nextUntil(nextHeader).last().append("<p class='closeLinkClass' onclick='document.getElementById(\""+div[0]+"\").style.display=\"none\";'><a href='"+div[0]+"'>"+linkText+"</a></p>");
+	$(h[1]).nextUntil(nextHeader).last().append("<p class='closeLinkClass' onclick='document.getElementById(\""+div[1]+"\").style.display=\"none\";'><a href='"+div[1]+"'>"+linkText+"</a></p>");
+	$(h[2]).nextUntil(nextHeader).last().append("<p class='closeLinkClass' onclick='document.getElementById(\""+div[2]+"\").style.display=\"none\";'><a href='"+div[2]+"'>"+linkText+"</a></p>");
+	$(h[3]).nextUntil(nextHeader).last().append("<p class='closeLinkClass' onclick='document.getElementById(\""+div[3]+"\").style.display=\"none\";'><a href='"+div[3]+"'>"+linkText+"</a></p>");
+	$(h[4]).nextUntil(nextHeader).last().append("<p class='closeLinkClass' onclick='document.getElementById(\""+div[4]+"\").style.display=\"none\";'><a href='"+div[4]+"'>"+linkText+"</a></p>");
+	$(h[5]).nextUntil(nextHeader).last().append("<p class='closeLinkClass' onclick='document.getElementById(\""+div[5]+"\").style.display=\"none\";'><a href='"+div[5]+"'>"+linkText+"</a></p>");
+	$(h[6]).nextUntil(nextHeader).last().append("<p class='closeLinkClass' onclick='document.getElementById(\""+div[6]+"\").style.display=\"none\";'><a href='"+div[6]+"'>"+linkText+"</a></p>");
+	$(h[7]).nextUntil(nextHeader).last().append("<p class='closeLinkClass' onclick='document.getElementById(\""+div[7]+"\").style.display=\"none\";'><a href='"+div[7]+"'>"+linkText+"</a></p>");
+	$(h[8]).nextUntil(nextHeader).last().append("<p class='closeLinkClass' onclick='document.getElementById(\""+div[8]+"\").style.display=\"none\";'><a href='"+div[8]+"'>"+linkText+"</a></p>");
+	$(h[9]).nextUntil(nextHeader).last().append("<p class='closeLinkClass' onclick='document.getElementById(\""+div[9]+"\").style.display=\"none\";'><a href='"+div[9]+"'>"+linkText+"</a></p>");
+	$(h[10]).nextUntil(nextHeader).last().append("<p class='closeLinkClass' onclick='document.getElementById(\""+div[10]+"\").style.display=\"none\";'><a href='"+div[10]+"'>"+linkText+"</a></p>");
+	$(h[11]).nextUntil(nextHeader).last().append("<p class='closeLinkClass' onclick='document.getElementById(\""+div[11]+"\").style.display=\"none\";'><a href='"+div[11]+"'>"+linkText+"</a></p>");
 	
 
 	console.log("END AtCoder-temp.js");
