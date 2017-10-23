f75548375ee4071a684423b9f9b45cfb6bc56d07
neetaro <neet.utsu.taro@gmail.com>
2017-10-23 14:34:10 +0900

    a

--- gist-next-hidden.js
+++ gist-next-hidden.js
@@ -1,6 +1,10 @@
 $(function(){
 	var plainCode = "pre.code";
 	$(".gist").next(plainCode).hide().end().parent().parent().next(plainCode).hide();
-	$("code[data-gist-id]").next(plainCode).hide();
-	$("code[data-gist-id]").parent.next(plainCode).hide();
+	$("code[data-gist-id]").next(plainCode).hide().end().parent.next(plainCode).hide();
+	var preTag  = $(plainCode);
+	if(preTag.prev().find(".gist,code[data-gist-id]")){
+		preTag.hide();
+	}
+
 })
